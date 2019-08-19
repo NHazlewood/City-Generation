d0698119a513c31bdbd1b17fb1f9be1ee38cd0a5
edward <edward@pdx.edu>
2019-08-18 19:11:56 -0700

    a few changes
    a few small changes and tests

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <body>
 
-    <form action="index.php" method="post">
-        X: <input type="number" name="x"> <br>
-        Y: <input  type="number" name="y"> <br>
+    <form action="" method="post">
+        Width: <input type="number" name="x"> <br>
+        Height: <input  type="number" name="y"> <br>
         SEED: <input  type="number" name="seed"> <br>
         <input type="submit">
     </form>
@@ -13,17 +13,29 @@
         $x = $_POST["x"];
         $y = $_POST["y"];
         $seed = $_POST["seed"];
+        $image = imagecreatetruecolor($x, $y);
+        //phpinfo();
+        //imagejpeg($image);
 
         function setMap(){
             $x = $GLOBALS['x'];
             $y = $GLOBALS['y'];
             $map =  array_fill(0,$x,NULL);
-            echo "test $x, $y";
+            //echo "test $x, $y";
             for($i=0; $i < $x; ++$i){
                 $map[$i] = array_fill(0,$y,array('nothing',0));
-                echo "test $i <br>";
+                //echo "test $i <br>";
             }
             $GLOBALS['map'] = $map;
+        }
+
+        function setSeed(){
+            if($GLOBALS['seed'] == NULL) {
+                $GLOBALS['seed'] = rand();
+            }
+            srand($GLOBALS['seed']);
+            $test = rand();
+            //echo "test: $test <br>";
         }
 
         function applyMajorRoads(){
@@ -51,6 +63,7 @@
             growRoads();
         }
 
+
         function exportMapToImage(){
             //convert $map from type and weights into rgb
             //create image
@@ -65,12 +78,19 @@
         
     
         //echo "$x x $y <br>";
+        setSeed();
         setMap();
-        //check if there is alreadya  saved map for that seed, if so just load
+        //check if there is already a saved map for that seed, if so just load
         applyMajorRoads();
         applyMinorRoads();
         exportMapToImage();
         displayMapToUser();
+
+        //takes $image and turns it into jpeg
+        //imagejpeg($image);
+        
+        
+        echo "Seed: $seed <br>";
 
         //print_r($map);
         //echo $map[0][0]. "";
